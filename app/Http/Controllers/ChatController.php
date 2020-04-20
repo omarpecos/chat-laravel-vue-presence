55ef0beda475288fe62db2a85e5cac63f4a68282
omarpecos <omarpecos@gmail.com>
2020-04-20 15:53:55 +0200

    blogChat with msg on BD

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Chat;
+use App\Events\ChatEvent;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+    	return view('chat.chat');
+    }
+
+    public function fetchAllMessages()
+    {
+    	return Chat::with('user')->get();
+    }
+
+    public function sendMessage(request $request)
+    {
+
+       //return $request->all();
+
+    	$chat = auth()->user()->messages()->create([
+            'message' => $request->message
+        ]);
+
+    	broadcast(new ChatEvent($chat->load('user')))->toOthers();
+        //event(new ChatEvent($chat->load('user')));
+
+    	return ['status' => 'success'];
+    }
+}
